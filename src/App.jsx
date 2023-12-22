0272fb55c77b996e5aba07968ea4aff0da16b7a5
Ujef55 <ujef.kelawala123@gmail.com>
2023-12-23 04:00:14 +0530

    minor tailwind css and jsx issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import NewProject from "./Components/NewProject";
 function App() {
   return (
     <>
-      <main className="h-screen my-8">
+      <main className="h-screen my-8 flex gap-8">
           <ProjectSidebar />
+          <NewProject />
       </main>
-      <NewProject />
     </>
   );
 }
