d670ba8d8d0a37fe1cdb9bfc096cdc29472958e3
Ujef55 <ujef.kelawala123@gmail.com>
2023-12-25 17:55:35 +0530

    Adding selected project component

---
+++ src/Components/SelectedProject.jsx
@@ -0,0 +1,16 @@
+function selectedProject() {
+    return (
+        <div>
+            <header>
+                <div>
+                    <h1>title</h1>
+                    <button>Delete</button>
+                </div>
+                <p>Date</p>
+                <p>description</p>
+            </header>
+        </div>
+    )
+}
+
+export default selectedProject;
