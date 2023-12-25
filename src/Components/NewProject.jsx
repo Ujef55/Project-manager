c6e7fac05d8d2da90ef4ca4ba85e955586ff3b9e
Ujef55 <ujef.kelawala123@gmail.com>
2023-12-25 17:10:09 +0530

    Showing modal for incorrect entered values

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,7 +1,10 @@
 import Input from "./Input";
 import { useRef } from "react";
+import Modal from "./Modal";
 
 function NewProject ( {onAdd} ) {
+
+    const modal = useRef();
 
     const titleRef = useRef();
     const descriptionRef = useRef();
@@ -12,6 +15,15 @@ function NewProject ( {onAdd} ) {
         const enteredDescription = descriptionRef.current.value;
         const enteredDueDate = dueDateRef.current.value;
 
+        if(
+            enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim() === ''
+        ) {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -19,7 +31,18 @@ function NewProject ( {onAdd} ) {
         });
     }
 
-    return (
+    return <>
+
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className="text-stone-600 mb-4">
+                Oops ... looks like you forgot to enter a value.
+            </p>
+            <p className="text-stone-600 mb-4">
+                Please make sure you provide a valid value for every input field.
+            </p>
+        </Modal>
+
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
@@ -35,7 +58,7 @@ function NewProject ( {onAdd} ) {
                 <Input type='date' ref={dueDateRef} label={'Due Date'} />
             </div>
         </div>
-    )
+        </>
 }
 
 export default NewProject;
